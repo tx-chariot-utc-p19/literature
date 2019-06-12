1f42524f75a58f9f999c15273c39b1cba9d397fb
CdRom1 <r.maliach@live.fr>
2019-06-12 19:32:15 +0200

    Ajout d'un template avec du lorem ipsum

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+pdf="pdflatex -shell-escape main.tex"
+bib="biber main"
+if [ "$1" = "bib" ]; then
+echo "compiler avec biblio!"
+$pdf
+$bib
+$pdf
+else
+echo "compiler sans biblio!"
+$pdf
+fi;
